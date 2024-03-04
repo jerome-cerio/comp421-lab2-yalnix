a15073bc6c1deca16afe229f51fe181df6dfd42d
SamYu0310 <111807975+SamYu0310@users.noreply.github.com>
2024-03-04 16:16:33 -0600

    interrupt vector table

--- yalnix.c
+++ yalnix.c
@@ -31,7 +31,28 @@ SavedContext *MyCFunc(SavedContext *ctxp, void *p1, void *p2);
 
 void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, char **cmd_args) {
 
-    // Initialize structures
+    // Initialize interrupt vector table
+    void (*interrupt_vector_table[TRAP_VECTOR_SIZE])(ExceptionInfo *info); 
+
+    interrupt_vector_table[0] = trap_kernel_handler; 
+    interrupt_vector_table[1] = trap_clock_handler; 
+    interrupt_vector_table[2] = trap_illegal_handler; 
+    interrupt_vector_table[3] = trap_memory_handler; 
+    interrupt_vector_table[4] = trap_math_handler; 
+    interrupt_vector_table[5] =  trap_tty_receive_handler; 
+    interrupt_vector_table[6] = trap_tty_transmit_handler; 
+    interrupt_vector_table[7] = null; 
+    interrupt_vector_table[8] = null; 
+    interrupt_vector_table[9] = null; 
+    interrupt_vector_table[10] = null; 
+    interrupt_vector_table[11] = null; 
+    interrupt_vector_table[12] = null; 
+    interrupt_vector_table[13] = null; 
+    interrupt_vector_table[14] = null; 
+    interrupt_vector_table[15] = null; 
+
+    // Initialize the REG_VECTOR_BASE to point to interrupt vector table 
+    WriteRegister(REG_VECTOR_BASE, (RCS421RegVal) interrupt_vector_table); 
 
     // Create the idle process - pid0
 
