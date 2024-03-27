0990cf3be563b5a289fb90509fcbe70c49c9d953
Jerome Cerio <jerome.ceriojr@gmail.com>
2024-03-27 15:42:05 -0500

    updated memory structure. added trackers for free pages

--- memorymanagement.c
+++ memorymanagement.c
@@ -1,6 +1,8 @@
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
 
+#include "memorymanagement.h"
+
 #define VALID   1 
 #define INVALID 0
 
@@ -16,14 +18,11 @@ struct pte *region1_pt;
 // Kernel break
 void *curr_kernel_brk = VMEM_1_BASE;
 
-// Boolean to track whether virtual memory has been enabled or not
-int vm_enabled = 0;
-
 int num_physical_pages;
-int num_free_pages;
 
 // head of the free page list
 struct free_list_entry *free_list_head;
+
 
 int SetKernelBrk(void *addr) {
 
@@ -40,39 +39,53 @@ void* getKernelBrk() {
     return curr_kernel_brk;
 }
 
-// void initFreePagesList(unsigned int pmem_size) {
+/** 
+ * This function obtains a free page from the free page list 
+ * */
 
-//     // Find the already used pages
-//     num_physical_pages = pmem_size / PAGESIZE;
-//     num_free_pages = 0;
+int GetFreePage () {
 
-//     int used_pages_min = DOWN_TO_PAGE(VMEM_1_BASE) / PAGESIZE;
-//     int used_pages_max = UP_TO_PAGE(curr_kernel_brk) / PAGESIZE;
+    // Gets the first available PFN from the list
+    int pfn = free_head;
 
-//     /* Create the free list*/
+    // Borrow the page table entry from the top of the region 1 page table
+    BorrowPTE();
+    pt1[borrowed_idx].pfn = pfn;
 
-//     // Head of the free list
-//     free_list_head = Malloc(sizeof(struct free_list_entry*));
-//     free_list_head->pfn = 0;
-//     free_list_head->next = NULL;
+    // Move to the next page in the list
+    unsigned int *addr = (unsigned int*) borrowed_addr;
+    free_head = *addr;
 
-//     // Make the rest of the free list
-//     for (int i = MEM_INVALID_PAGES; i < num_physical_pages; i++) {
-        
-//         if (i < used_pages_min || i > used_pages_max) {
-//             // The page is free. Add it to the free list
+    // Decrements the number of free pages
+    free_num_pages--;
 
-//             struct free_list_entry *new_pfn_entry = malloc(sizeof (struct free_list_entry*));
-//             if (new_pfn_entry == NULL) {
-//                 errorFunc();
-//             }
-//             new_pfn_entry->pfn = i;
-//             new_pfn_entry->next = free_list_head;
-//             free_list_head = new_pfn_entry;
-//             num_free_pages++;
-//         }
-//     }
-// } 
+    // Frees the borrowed PTE
+    ReleasePTE();
+
+    // Returns the PFN
+    return pfn;
+}
+
+/* Adds a page to the free page list */
+void AddFreePage (int index, int pfn) {
+
+    // Computes the virtual address of the page
+    unsigned int *addr = (unsigned int*)((uintptr_t) VMEM_0_BASE + index * PAGESIZE);
+
+    // Flushes the address from the TLB
+    WriteRegister(REG_TLB_FLUSH, (RCS421RegVal) addr);
+
+    // Adds the page to the list
+    *addr = free_head;
+    free_head = pfn;
+
+    // Increments the number of free pages
+    num_free_pages++;
+}
+
+/**
+ * PAGE TABLE HANDLING
+*/
 
 void initPageTables() {
     region0_pt = VMEM_1_LIMIT - PAGESIZE; 
@@ -134,3 +147,42 @@ void enable_VM() {
     vm_enabled = 1;
 }
 
+/* Borrows a new PTE from the region 1 page table */
+void BorrowPTE () {
+
+    // Updates the borrowed address and index
+    borrowed_addr = (void*)((uintptr_t) borrowed_addr - PAGESIZE);
+    borrowed_idx--;
+
+    // Temporarily buffers the current PTE if in use
+    if(pt1[borrowed_idx].valid) {
+        pte_buffer[pte_count] = pt1[borrowed_idx];
+        pte_count++;
+
+        // Flushes the invalidated TLB entry
+        WriteRegister(REG_TLB_FLUSH, (RCS421RegVal) borrowed_addr);
+    }
+
+    // Initializes the PTE
+    pt1[borrowed_idx].valid = 1;
+    pt1[borrowed_idx].kprot = PROT_READ | PROT_WRITE;
+}
+
+
+/* Frees a PTE borrowed from the region 1 page table */
+void ReleasePTE () {
+
+    // Frees the PTE
+    pt1[borrowed_idx].valid = 0;
+
+    // Restores a buffered PTE
+    if(pte_count) {
+        pte_count--;
+        pt1[borrowed_idx] = pte_buffer[pte_count];
+    }
+    WriteRegister(REG_TLB_FLUSH, (RCS421RegVal) borrowed_addr);
+
+    // Updates the borrowed address and index
+    borrowed_addr = (void*)((uintptr_t) borrowed_addr + PAGESIZE);
+    borrowed_idx++;
+}
---
+++ memorymanagement.h
@@ -0,0 +1,43 @@
+#ifndef MEMORYMANAGEMENT_H
+#define MEMORYMANAGEMENT_H
+
+#include <comp421/hardware.h>
+#include <stdint.h>
+
+// Boolean to track whether virtual memory has been enabled or not
+int vm_enabled = 0;
+
+// Manages a list of free pages
+unsigned int free_head;
+unsigned int num_free_pages;
+
+// Manages free page tables
+int *free_tables;
+unsigned int num_free_tables;
+
+// Stores the address of the region 1 page table
+uintptr_t ptaddr1;
+struct pte *pt1;
+
+// Stores the address and index of a borrowed PTE
+void *borrowed_addr;
+int borrowed_idx;
+
+// Temporarily buffers borrowed PTEs if required
+struct pte pte_buffer[4];
+int pte_count;
+
+
+// Function prototypes
+int GetFreePage ();
+void AddFreePage (int , int );
+
+void BorrowPTE ();
+void ReleasePTE ();
+
+//uintptr_t GetPageTable ();
+void FreePageTable (uintptr_t );
+
+void CopyKernelStack (uintptr_t );
+
+#endif
